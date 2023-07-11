b0f5ab43190f54feb3fa56b6557ee6c6ed1a386d
NaoLizanoG <naomilizano@gmail.com>
2023-07-11 12:15:55 -0500

    Tarea 1, lectura del archivo completa

--- Ecuador.c
+++ Ecuador.c
@@ -2,7 +2,40 @@
 #include <stdio.h>
 #include <string.h>
 
+void leer (char Archivo[]){
+    char caracter;
+FILE*archivo;
+archivo=fopen(Archivo, "r");
+
+if(archivo==NULL){
+
+    printf("El archivo no se pudo abrir correctamente\n");
+}
+else{
+while (!feof(archivo))
+    {
+        caracter = fgetc(archivo);
+
+        if (caracter=='\n')
+        {
+            printf("\n");
+        }
+        else{
+        
+        putchar(caracter);
+        }
+
+    }
+    fclose(archivo);
+}
+}
+
+
 int main(){
+FILE *archivo;
+char ArchivoNuevo[20]="tricolor2.txt";
+char ArchivoViejo[20]="tricolor.txt";
+leer(ArchivoViejo);
 
     return 0;
 }
