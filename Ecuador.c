3016afcd5596138580b81b9a94134414fefe5a66
NaoLizanoG <naomilizano@gmail.com>
2023-07-11 12:58:45 -0500

    Version final

--- Ecuador.c
+++ Ecuador.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-//funcion para leer el texto y escribirlo en un nuevo archivo
+// funcion para leer el texto y escribirlo en un nuevo archivo
 void leer(char Archivo[], char OtroArchivo[])
 {
     char caracter;
@@ -39,12 +39,12 @@ void leer(char Archivo[], char OtroArchivo[])
     }
 }
 
-//funcion para encontrar los errores
-int encontrar(char nombreArchivo[])
+// funcion para encontrar los errores
+long encontrar(char nombreArchivo[])
 {
-    int num = 0;
     long posicion;
-    char caracter[10], palabra[] = "ecuador";
+    int num;
+    char caracter[10], palabra[10] = "ecuador";
     FILE *archivo;
     archivo = fopen(nombreArchivo, "r+");
     if (archivo == NULL)
@@ -66,22 +66,75 @@ int encontrar(char nombreArchivo[])
         printf("Se encontraron %d errores\n", num);
     }
     fclose(archivo);
-    return num;
+    return posicion;
 }
 
 
-//funcion general
+int errores(char nombreArchivo[])
+{
+    long posicion;
+    int num;
+    char caracter[10], palabra[10] = "ecuador";
+    FILE *archivo;
+    archivo = fopen(nombreArchivo, "r+");
+    if (archivo == NULL)
+    {
+
+        printf("El archivo no se pudo abrir correctamente\n");
+    }
+    else
+    {
+        while (!feof(archivo))
+        {
+            posicion = ftell(archivo);
+            fscanf(archivo, "%s", caracter);
+            if (strcmp(palabra, caracter) == 0)
+            {
+                num = num + 1;
+            }
+        }
+    }
+    fclose(archivo);
+    return num;
+}
+
+//funcion para reemplazar 
+void reemplazar(char Archivo[], long posicion)
+{
+    char caracter[20], nuevo[20]="Ecuador" ;
+    FILE *archivo;
+    archivo = fopen(Archivo, "r");
+    if (archivo == NULL)
+    {
+
+        printf("El archivo no se pudo abrir correctamente\n");
+    }
+    else
+    {
+        fseek(archivo, posicion, 0);
+        fscanf(archivo, " %s ", &caracter);
+        fseek(archivo, posicion, 0);
+        fprintf(archivo, " %s ", nuevo);
+
+    }
+     fclose(archivo);
+}
+
+// funcion general
 int main()
 {
-    FILE *archivo;
     int num;
+    FILE *archivo;
+    long posicion;
     char ArchivoNuevo[20] = "tricolor2.txt";
     char ArchivoViejo[20] = "tricolor.txt";
     archivo = fopen(ArchivoNuevo, "a+");
     fclose(archivo);
     leer(ArchivoViejo, ArchivoNuevo);
-    num=encontrar(ArchivoNuevo);
-    printf("Se encontraron %d errores\n",num );
+    posicion = encontrar(ArchivoNuevo);
 
+    reemplazar(ArchivoNuevo, posicion);
+    num=errores(ArchivoNuevo);
+    printf("Hay %d errores", num);
     return 0;
 }
